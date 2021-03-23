b09ffa6155f7ecf94211865a13ce49ccaf82194b
nickz <nick.zubrzycki@skynet.be>
2021-03-23 09:00:58 +0100

    Add project files.

---
+++ Book/Book.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    class Book
+    {
+        public int ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        private double price;
+        public virtual double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public static Book operator +(Book a, Book b)
+        {
+            Book omnibus = new Book();
+            omnibus.ISBN = a.ISBN + b.ISBN;
+            omnibus.Price = (a.Price + b.Price) / 2;
+            omnibus.Title = $"Omnibus van [{a.Author}, {b.Author}]";
+            return omnibus;
+        }
+        public override string ToString()
+        {
+            return $"{Title} - {Author} ({ISBN}) {Price}";
+        }
+        public override bool Equals(object obj)
+        {
+            Book temp = (Book)obj;
+            return (ISBN == temp.ISBN);
+        }
+        public override int GetHashCode()
+        {
+            return ISBN;
+        }
+
+    }
+    class TextBook : Book
+    {
+        public int GradeLevel { get; set; }
+        public override double Price
+        {
+            get { return base.Price; }
+            set
+            {
+                if (value >= 20 && value <= 80)
+                    base.Price = value;
+                else
+                    base.Price = 0;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Title} - {Author} ({ISBN}) (Grade Level {GradeLevel} {Price}";
+        }
+
+    }
+    class CoffeeTableBook : Book
+    {
+        public override double Price
+        {
+            get { return base.Price; }
+            set
+            {
+                if (value >= 35 && value <= 100)
+                    base.Price = value;
+                else
+                    base.Price = 0;
+            }
+        }
+    }
+}
+}
---
+++ Book/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book boek1 = new Book();
+            boek1.Title = "The Shining";
+            boek1.Author = "Stephen King";
+            boek1.ISBN = 05848152;
+            boek1.Price = 25;
+            Book boek2 = new Book();
+            boek2.Title = "The Haunting of Hill House";
+            boek2.Author = "Shirley Jackson";
+            boek2.ISBN = 05848153;
+            boek2.Price = 20;
+            TextBook tb = new TextBook();
+            tb.Title = "Zie Scherp";
+            tb.Author = "Tim Dams";
+            tb.ISBN = 03449664;
+            tb.Price = 15;
+            tb.GradeLevel = 4;
+            CoffeeTableBook ctb = new CoffeeTableBook();
+            ctb.Title = "Vlaamse Primitieven";
+            ctb.Author = "Johannes De Meester";
+            ctb.ISBN = 01134685;
+            ctb.Price = 85;
+
+            Book omnibus = boek1 + boek2;
+            Console.WriteLine(omnibus);
+            Console.WriteLine(tb);
+            Console.WriteLine(ctb);
+            Console.WriteLine(tb.Equals(ctb));
+
+
+            Console.ReadLine();
+        }
+    }
+}
